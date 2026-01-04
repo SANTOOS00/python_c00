05268f58811cabde408724bfd15d66b130a5d8ae
Kh4wf0x <mohamederraiss@icloud.com>
2026-01-04 06:17:09 +0200

    is not finich :)

--- py_01/ex1/ft_garden_data.py
+++ py_01/ex1/ft_garden_data.py
@@ -6,7 +6,7 @@
 #    By: santoos <santoos@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/30 22:34:02 by moerrais          #+#    #+#              #
-#    Updated: 2026/01/03 07:20:02 by santoos          ###   ########.fr        #
+#    Updated: 2026/01/04 03:12:28 by santoos          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,11 +16,15 @@ class Plant():
         self.name = name
         self.height = height
         self.age = age
+
     def print_data(self):
         print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
 data1 = Plant("Rose", 25, 30)
 data2 = Plant("Sunflower", 80, 45)
 data3 = Plant("Cactus", 15, 120)
+
 
 data1.print_data()
 data2.print_data()
--- py_01/ex2/ft_plant_growth.py
+++ py_01/ex2/ft_plant_growth.py
@@ -3,10 +3,41 @@
 #                                                         :::      ::::::::    #
 #    ft_plant_growth.py                                 :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+         #
+#    By: santoos <santoos@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/30 20:18:22 by moerrais          #+#    #+#              #
-#    Updated: 2025/12/30 20:18:23 by moerrais         ###   ########.fr        #
+#    Updated: 2026/01/04 06:16:39 by santoos          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+class Plant():
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+    
+    def grow(self):
+        if self.age <= 60:
+            self.height += 1
+        else:
+            self.height += 0.5
+    
+    def age_hani(self):
+        self.age += 1
+    
+    def get_info(self):
+        return f"{self.name}: {self.height}cm, {self.age} days old"
+    
+    def simulate_week(plant):
+        tmp = plant.height
+        print ("== Day 1 ==")
+        print (plant.get_info())
+        for _ in range(6):
+            plant.age_hani()
+            plant.grow()
+        print ("== Day 7 ==")
+        print (plant.get_info())
+        print (f"Growth this week: +{plant.height - tmp}cm")
+
+data1 = Plant("Rose", 25, 30)
+data1.simulate_week()
